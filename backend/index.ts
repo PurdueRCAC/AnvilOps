70392e691aac15bf0162b2b604b9af168e85f50e
swans150 <swans150@purdue.edu>
2025-05-29 10:24:04 -0400

    Respond with the contents of index.html when an HTML request doesn't match any file in the public directory

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,25 @@
 import express from "express";
 import { existsSync, statSync } from "node:fs";
+import path from "node:path";
 
 const app = express();
 const port = 3000;
 
 if (existsSync("./public") && statSync("./public").isDirectory()) {
   console.log("Serving static files from ./public");
+  const index = path.resolve(import.meta.dirname, "public/index.html");
+
   app.use(express.static("./public"));
+  app.use((req, res, next) => {
+    if (
+      (req.method === "GET" || req.method === "HEAD") &&
+      req.accepts("html")
+    ) {
+      res.sendFile(index, (err) => err && next());
+    } else {
+      next();
+    }
+  });
 }
 
 app.listen(port, () => {
