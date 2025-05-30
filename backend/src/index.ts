47da7d595c88c75b1a43ca0f3479b7052beb0ea5
swans150 <swans150@purdue.edu>
2025-05-30 09:57:11 -0400

    Fix serving static files from the public directory

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,13 @@ import path from "node:path";
 const app = express();
 const port = 3000;
 
-if (existsSync("./public") && statSync("./public").isDirectory()) {
-  console.log("Serving static files from ./public");
-  const index = path.resolve(import.meta.dirname, "public/index.html");
+const publicDir = path.resolve(path.dirname(import.meta.dirname), "public");
 
-  app.use(express.static("./public"));
+if (existsSync(publicDir) && statSync(publicDir).isDirectory()) {
+  console.log("Serving static files from", publicDir);
+  const index = path.resolve(publicDir, "index.html");
+
+  app.use(express.static(publicDir));
   app.use((req, res, next) => {
     if (
       (req.method === "GET" || req.method === "HEAD") &&
