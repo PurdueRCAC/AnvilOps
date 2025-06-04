ff16de4cc576eb9b853586358a08529fb43463cd
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2025-06-04 10:45:49 -0400

    openid auth flow with postgres session store

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,54 @@
 import express from "express";
 import { existsSync, statSync } from "node:fs";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import getProtectedApiRouter, {SESSION_COOKIE_NAME } from './lib/auth.ts';
 import path from "node:path";
-import { OpenAPIBackend, type Request } from "openapi-backend";
-import addFormats from "ajv-formats";
-import handlers from "./lib/handlers.ts";
+import dotenv from "dotenv";
+import apiHandler from "./lib/api.ts";
+import connectPgSimple from "connect-pg-simple";
+
+dotenv.config();
+
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    throw new Error("Credentials not set");
+}
+
+if (!process.env.SESSION_SECRET) {
+  throw new Error("Session secret not set");
+}
+
+const DB_URL = process.env.DATABASE_URL ?? `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOSTNAME}/${process.env.POSTGRES_DB}`;
+if (!DB_URL) {
+  throw new Error("Database credentials not set");
+}
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-const api = new OpenAPIBackend({
-  definition: path.resolve(
-    path.dirname(path.dirname(import.meta.dirname)),
-    "openapi",
-    "openapi.yaml"
-  ),
-  handlers,
-  ajvOpts: { coerceTypes: "array" },
-  customizeAjv: (ajv) => {
-    addFormats(ajv, { mode: 'fast', formats: ['email', 'uri', 'date-time', 'uuid', 'int64']});
-    return ajv;
-  }
-});
+app.use(cookieParser());
 
-app.use("/api", (req, res) => {
-  api.handleRequest(req as Request, req, res);
-});
+const PgSession = connectPgSimple(session);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  name: SESSION_COOKIE_NAME,
+  cookie: {
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: 'lax',
+    maxAge: 18 * 60 * 60 * 1000, // 18 hr
+    httpOnly: true,
+  },
+  store: new PgSession({
+    conString: DB_URL,
+  })
+}));
+
+const apiRouter = await getProtectedApiRouter();
+apiRouter.use(apiHandler);
+app.use('/api', apiRouter);
 
 const publicDir = path.resolve(path.dirname(import.meta.dirname), "public");
 if (existsSync(publicDir) && statSync(publicDir).isDirectory()) {
