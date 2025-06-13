811da83a9ed74d7caa444d6cf65f0d666f31ed7a
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2025-06-13 08:49:24 -0400

    Updated middleware for new database schema

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "../generated/prisma/client.ts";
 import crypto, { createCipheriv, createDecipheriv } from "node:crypto";
-import { type StringFieldUpdateOperationsInput } from "../generated/prisma/models.ts";
 
 export const DATABASE_URL =
   process.env.DATABASE_URL ??
@@ -9,51 +8,45 @@ export const DATABASE_URL =
 const masterKey = Buffer.from(process.env.FIELD_ENCRYPTION_KEY, "base64");
 const algorithm = "aes-256-gcm";
 const separator = "|";
+const contentSeparator = "$";
 
-export const encryptKey = (key: Buffer) => {
+const encrypt = (secret: string, key: Buffer): string => {
   const iv = crypto.randomBytes(12);
-  const cipher = createCipheriv(algorithm, masterKey, iv);
-  const res = Buffer.concat([cipher.update(key), cipher.final()]);
+  const cipher = createCipheriv(algorithm, key, iv);
+  const res = Buffer.concat([cipher.update(secret), cipher.final()]);
   const authTag = cipher.getAuthTag();
   return [authTag, iv, res]
     .map((buf) => buf.toString("base64"))
     .join(separator);
 };
 
-const decryptKey = (keyCtxt: string | StringFieldUpdateOperationsInput) => {
-  const key = typeof keyCtxt === "string" ? keyCtxt : keyCtxt.set;
-  const [authTagEncoded, ivEncoded, ctxtEncoded] = key.split("|");
+const decrypt = (ctxtFull: string, key: Buffer): string => {
+  const [authTagEncoded, ivEncoded, ctxtEncoded] = ctxtFull.split("|");
   const iv = Buffer.from(ivEncoded, "base64");
   const ctxt = Buffer.from(ctxtEncoded, "base64");
   const authTag = Buffer.from(authTagEncoded, "base64");
-  const decipher = createDecipheriv(algorithm, masterKey, iv);
-  decipher.setAuthTag(authTag);
-
-  return Buffer.concat([decipher.update(ctxt), decipher.final()]);
-};
-
-const encrypt = (
-  text: string,
-  keyCtxt: string | StringFieldUpdateOperationsInput,
-) => {
-  const iv = crypto.randomBytes(12);
-  const key = decryptKey(keyCtxt);
-  const cipher = createCipheriv(algorithm, key, iv);
-  const res = Buffer.concat([cipher.update(text, "utf8"), cipher.final()]);
-  const authTag = cipher.getAuthTag();
-  return [authTag, iv, res].map((buf) => buf.toString("base64")).join("|");
-};
-
-const decrypt = (secret: string, keyCtxt: string) => {
-  const [authTagEncoded, ivEncoded, ctxtEncoded] = secret.split("|");
-  const authTag = Buffer.from(authTagEncoded, "base64");
-  const iv = Buffer.from(ivEncoded, "base64");
-  const key = decryptKey(keyCtxt);
   const decipher = createDecipheriv(algorithm, key, iv);
   decipher.setAuthTag(authTag);
-  const res =
-    decipher.update(ctxtEncoded, "base64", "utf8") + decipher.final("utf8");
-  return res;
+
+  return decipher.update(ctxt, undefined, "utf8") + decipher.final("utf8");
+};
+
+const genKey = (): Buffer => {
+  return crypto.randomBytes(32);
+};
+
+const encryptSecret = (secret: string): string => {
+  const key = genKey();
+  const enc = encrypt(secret, key);
+  const keyCtxt = encrypt(key.toString("base64"), masterKey);
+  return keyCtxt + contentSeparator + enc;
+};
+
+const decryptSecret = (secret: string): string => {
+  const [keyCtxt, enc] = secret.split(contentSeparator);
+  const keyEncoded = decrypt(keyCtxt, masterKey);
+  const key = Buffer.from(keyEncoded, "base64");
+  return decrypt(enc, key);
 };
 
 const client = new PrismaClient({
@@ -62,21 +55,18 @@ const client = new PrismaClient({
     deployment: {
       secret: true,
     },
-    app: {
-      secretKeyCtxt: true,
-    },
   },
 });
 
 export const db = client.$extends({
   name: "Secret encryption and decryption",
   result: {
-    app: {
+    deploymentConfig: {
       secrets: {
-        needs: { secrets: true, secretKeyCtxt: true },
-        compute(app) {
+        needs: { secrets: true },
+        compute(dc) {
           try {
-            return decrypt(app.secrets, app.secretKeyCtxt);
+            return decryptSecret(dc.secrets);
           } catch (err) {
             console.error(err);
             return "";
@@ -87,26 +77,40 @@ export const db = client.$extends({
   },
 
   query: {
-    app: {
+    deployment: {
+      async create({ args, query }) {
+        if (!args.data.config) {
+          return query(args);
+        }
+
+        const createConfig = args.data.config.create;
+        if (createConfig && createConfig.secrets) {
+          createConfig.secrets = encryptSecret(createConfig.secrets);
+        }
+
+        const connectConfig = args.data.config.connectOrCreate;
+        if (connectConfig && connectConfig.create.secrets) {
+          connectConfig.create.secrets = encryptSecret(
+            connectConfig.create.secrets,
+          );
+        }
+
+        return query(args);
+      },
+    },
+
+    deploymentConfig: {
       async $allOperations({ operation, args, query }) {
         switch (operation) {
           case "update":
           case "updateMany":
           case "updateManyAndReturn":
           case "create":
-            args.data.secretKeyCtxt = encryptKey(crypto.randomBytes(32));
-
             if (args.data.secrets) {
               if (typeof args.data.secrets === "string") {
-                args.data.secrets = encrypt(
-                  args.data.secrets,
-                  args.data.secretKeyCtxt,
-                );
+                args.data.secrets = encryptSecret(args.data.secrets);
               } else if (args.data.secrets.set === "string") {
-                args.data.secrets.set = encrypt(
-                  args.data.secrets.set,
-                  args.data.secretKeyCtxt,
-                );
+                args.data.secrets.set = encryptSecret(args.data.secrets.set);
               }
             }
             break;
@@ -114,42 +118,24 @@ export const db = client.$extends({
           case "createMany":
             if (args.data instanceof Array) {
               args.data.forEach((data) => {
-                data.secretKeyCtxt = encryptKey(crypto.randomBytes(32));
-
                 if (!data.secrets) return;
-                data.secrets = encrypt(data.secrets, data.secretKeyCtxt);
+                data.secrets = encryptSecret(data.secrets);
               });
             } else {
-              args.data.secretKeyCtxt = encryptKey(crypto.randomBytes(32));
-
               if (args.data.secrets) {
-                args.data.secrets = encrypt(
-                  args.data.secrets,
-                  args.data.secretKeyCtxt,
-                );
+                args.data.secrets = encryptSecret(args.data.secrets);
               }
             }
             break;
           case "upsert":
-            args.create.secretKeyCtxt = encryptKey(crypto.randomBytes(32));
-
             if (args.create.secrets) {
-              args.create.secrets = encrypt(
-                args.create.secrets,
-                args.create.secretKeyCtxt,
-              );
+              args.create.secrets = encryptSecret(args.create.secrets);
             }
 
             if (typeof args.update.secrets === "string") {
-              args.update.secrets = encrypt(
-                args.update.secrets,
-                args.update.secretKeyCtxt,
-              );
+              args.update.secrets = encryptSecret(args.update.secrets);
             } else if (typeof args.update.secrets.set === "string") {
-              args.update.secrets.set = encrypt(
-                args.update.secrets.set,
-                args.update.secretKeyCtxt,
-              );
+              args.update.secrets.set = encryptSecret(args.update.secrets.set);
             }
             break;
         }
