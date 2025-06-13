491cd5acbc15312f9b7e347c6e82abc116031d1f
swans150 <swans150@purdue.edu>
2025-06-13 13:58:11 -0400

    Set deployment status to ERROR if the image build fails

--- builders/dockerfile/docker-entrypoint.sh
+++ builders/dockerfile/docker-entrypoint.sh
@@ -5,26 +5,36 @@
 
 set -eo pipefail
 
+set_status() {
+  wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"$1\"}" -O- $DEPLOYMENT_API_URL/deployment/update
+}
+
 cd /work
 
-wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"BUILDING\"}" -O- $DEPLOYMENT_API_URL/deployment/update
+set_status BUILDING
 
 git clone $CLONE_URL --depth=1 .
 
 cd "$ROOT_DIRECTORY"
 
-buildctl \
- --addr=tcp://buildkitd:1234 \
- --wait \
- --tlsdir /certs \
- --debug \
- build \
- --frontend dockerfile.v0 \
- --local context=. \
- --local dockerfile="$DOCKERFILE_PATH" \
- --import-cache type=registry,ref=$CACHE_TAG \
- --export-cache type=registry,ref=$CACHE_TAG \
- --output type=image,name=$IMAGE_TAG,push=true \
- --progress plain
+build() {
+  buildctl \
+  --addr=tcp://buildkitd:1234 \
+  --wait \
+  --tlsdir /certs \
+  --debug \
+  build \
+  --frontend dockerfile.v0 \
+  --local context=. \
+  --local dockerfile="$DOCKERFILE_PATH" \
+  --import-cache type=registry,ref=$CACHE_TAG \
+  --export-cache type=registry,ref=$CACHE_TAG \
+  --output type=image,name=$IMAGE_TAG,push=true \
+  --progress plain
+}
 
-wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"DEPLOYING\"}" -O- $DEPLOYMENT_API_URL/deployment/update
+if build ; then
+  set_status DEPLOYING
+else
+  set_status ERROR
+fi
--- builders/railpack/docker-entrypoint.sh
+++ builders/railpack/docker-entrypoint.sh
@@ -5,30 +5,39 @@
 
 set -eo pipefail
 
+set_status() {
+  wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"$1\"}" -O- $DEPLOYMENT_API_URL/deployment/update
+}
+
 cd /work
 
-wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"BUILDING\"}" -O- $DEPLOYMENT_API_URL/deployment/update
+set_status BUILDING
 
 git clone $CLONE_URL --depth=1 /work/repo
 
-# Railpack is a tool that generates BuildKit LLB from a repository by looking at the files it contains and making an educated guess on the repo's technologies, package managers, etc.
-# https://railpack.com/
-railpack prepare "/work/repo/$ROOT_DIRECTORY" --plan-out /work/railpack-plan.json --info-out /work/railpack-info.json
+build() {
+  # Railpack is a tool that generates BuildKit LLB from a repository by looking at the files it contains and making an educated guess on the repo's technologies, package managers, etc.
+  # https://railpack.com/
+  railpack prepare "/work/repo/$ROOT_DIRECTORY" --plan-out /work/railpack-plan.json --info-out /work/railpack-info.json &&
+  
+  # https://railpack.com/guides/running-railpack-in-production/#building-with-buildkit
+  buildctl \
+    --addr=tcp://buildkitd:1234 \
+    --wait \
+    --tlsdir /certs \
+    build \
+    --frontend gateway.v0 \
+    --opt source=registry.anvil.rcac.purdue.edu/anvilops/railpack-frontend:latest \
+    --local "context=/work/repo/$ROOT_DIRECTORY" \
+    --local dockerfile=/work \
+    --export-cache type=registry,ref=$CACHE_TAG \
+    --import-cache type=registry,ref=$CACHE_TAG \
+    --output type=image,name=$IMAGE_TAG,push=true
+  # TODO: when adding support for secrets, remember to invalidate the cache when their values change: https://railpack.com/guides/running-railpack-in-production/#layer-invalidation
+}
 
-# https://railpack.com/guides/running-railpack-in-production/#building-with-buildkit
-buildctl \
- --addr=tcp://buildkitd:1234 \
- --wait \
- --tlsdir /certs \
- build \
- --frontend gateway.v0 \
- --opt source=registry.anvil.rcac.purdue.edu/anvilops/railpack-frontend:latest \
- --local "context=/work/repo/$ROOT_DIRECTORY" \
- --local dockerfile=/work \
- --export-cache type=registry,ref=$CACHE_TAG \
- --import-cache type=registry,ref=$CACHE_TAG \
- --output type=image,name=$IMAGE_TAG,push=true
-
- # TODO: when adding support for secrets, remember to invalidate the cache when their values change: https://railpack.com/guides/running-railpack-in-production/#layer-invalidation
-
-wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"DEPLOYING\"}" -O- $DEPLOYMENT_API_URL/deployment/update
+if build ; then
+  set_status DEPLOYING
+else
+  set_status ERROR
+fi
