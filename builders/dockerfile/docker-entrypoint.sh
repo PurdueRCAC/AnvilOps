62c8eb3799c8638dc05f66b787f4f6d14c616d72
swans150 <swans150@purdue.edu>
2025-06-13 16:34:07 -0400

    Set build status to ERROR if a build script error occurs outside of building the image (e.g. in the clone step)

--- builders/dockerfile/docker-entrypoint.sh
+++ builders/dockerfile/docker-entrypoint.sh
@@ -9,32 +9,39 @@ set_status() {
   wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"$1\"}" -O- "$DEPLOYMENT_API_URL/deployment/update"
 }
 
-cd /work
+run_job() {
+  cd /work
 
-set_status "BUILDING"
+  set_status "BUILDING"
 
-git clone "$CLONE_URL" --depth=1 --shallow-submodules --revision="$REF" .
+  git clone "$CLONE_URL" --depth=1 --shallow-submodules --revision="$REF" .
 
-cd "$ROOT_DIRECTORY"
+  cd "$ROOT_DIRECTORY"
 
-build() {
-  buildctl \
-  --addr=tcp://buildkitd:1234 \
-  --wait \
-  --tlsdir /certs \
-  --debug \
-  build \
-  --frontend dockerfile.v0 \
-  --local context=. \
-  --local dockerfile="$DOCKERFILE_PATH" \
-  --import-cache type=registry,ref="$CACHE_TAG" \
-  --export-cache type=registry,ref="$CACHE_TAG" \
-  --output type=image,name="$IMAGE_TAG",push=true \
-  --progress plain
+  build() {
+    buildctl \
+    --addr=tcp://buildkitd:1234 \
+    --wait \
+    --tlsdir /certs \
+    --debug \
+    build \
+    --frontend dockerfile.v0 \
+    --local context=. \
+    --local dockerfile="$DOCKERFILE_PATH" \
+    --import-cache type=registry,ref="$CACHE_TAG" \
+    --export-cache type=registry,ref="$CACHE_TAG" \
+    --output type=image,name="$IMAGE_TAG",push=true \
+    --progress plain
+  }
+
+  if build ; then
+    set_status "DEPLOYING"
+  else
+    set_status "ERROR"
+  fi
 }
 
-if build ; then
-  set_status "DEPLOYING"
-else
+if ! run_job ; then
   set_status "ERROR"
+  exit 1
 fi
--- builders/railpack/docker-entrypoint.sh
+++ builders/railpack/docker-entrypoint.sh
@@ -9,35 +9,42 @@ set_status() {
   wget --header="Content-Type: application/json" --post-data "{\"secret\":\"$DEPLOYMENT_API_SECRET\",\"status\":\"$1\"}" -O- "$DEPLOYMENT_API_URL/deployment/update"
 }
 
-cd /work
+run_job() {
+  cd /work
 
-set_status "BUILDING"
+  set_status "BUILDING"
 
-git clone "$CLONE_URL" --depth=1 --shallow-submodules --revision="$REF" /work/repo
+  git clone "$CLONE_URL" --depth=1 --shallow-submodules --revision="$REF" /work/repo
 
-build() {
-  # Railpack is a tool that generates BuildKit LLB from a repository by looking at the files it contains and making an educated guess on the repo's technologies, package managers, etc.
-  # https://railpack.com/
-  railpack prepare "/work/repo/$ROOT_DIRECTORY" --plan-out /work/railpack-plan.json --info-out /work/railpack-info.json &&
-  
-  # https://railpack.com/guides/running-railpack-in-production/#building-with-buildkit
-  buildctl \
-    --addr=tcp://buildkitd:1234 \
-    --wait \
-    --tlsdir /certs \
-    build \
-    --frontend gateway.v0 \
-    --opt source=registry.anvil.rcac.purdue.edu/anvilops/railpack-frontend:latest \
-    --local context="/work/repo/$ROOT_DIRECTORY" \
-    --local dockerfile=/work \
-    --export-cache type=registry,ref="$CACHE_TAG" \
-    --import-cache type=registry,ref="$CACHE_TAG" \
-    --output type=image,name="$IMAGE_TAG",push=true
-  # TODO: when adding support for secrets, remember to invalidate the cache when their values change: https://railpack.com/guides/running-railpack-in-production/#layer-invalidation
+  build() {
+    # Railpack is a tool that generates BuildKit LLB from a repository by looking at the files it contains and making an educated guess on the repo's technologies, package managers, etc.
+    # https://railpack.com/
+    railpack prepare "/work/repo/$ROOT_DIRECTORY" --plan-out /work/railpack-plan.json --info-out /work/railpack-info.json &&
+    
+    # https://railpack.com/guides/running-railpack-in-production/#building-with-buildkit
+    buildctl \
+      --addr=tcp://buildkitd:1234 \
+      --wait \
+      --tlsdir /certs \
+      build \
+      --frontend gateway.v0 \
+      --opt source=registry.anvil.rcac.purdue.edu/anvilops/railpack-frontend:latest \
+      --local context="/work/repo/$ROOT_DIRECTORY" \
+      --local dockerfile=/work \
+      --export-cache type=registry,ref="$CACHE_TAG" \
+      --import-cache type=registry,ref="$CACHE_TAG" \
+      --output type=image,name="$IMAGE_TAG",push=true
+    # TODO: when adding support for secrets, remember to invalidate the cache when their values change: https://railpack.com/guides/running-railpack-in-production/#layer-invalidation
+  }
+
+  if build ; then
+    set_status "DEPLOYING"
+  else
+    set_status "ERROR"
+  fi
 }
 
-if build ; then
-  set_status "DEPLOYING"
-else
+if ! run_job ; then
   set_status "ERROR"
+  exit 1
 fi
