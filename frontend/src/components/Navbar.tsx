c719cb2e33b58a6bad35443017cabf1ff911b82c
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2025-06-17 09:15:14 -0400

    Remove org dropdown and update profile menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,33 +1,19 @@
 import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import defaultPfp from "../assets/default_pfp.png";
 import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
 import { UserContext } from "./UserProvider";
 
 export default function Navbar() {
   const { user, loading } = useContext(UserContext);
-
-  const navigate = useNavigate();
-
-  const handleSelect = async (value: string) => {
-    const orgId = parseInt(value);
-    navigate(`/org/${orgId}`);
-  };
 
   return (
     <div className="sticky top-0 left-0 w-full flex justify-between items-center px-8 py-2 border-b gap-4 bg-white/50 backdrop-blur-xl h-16">
@@ -37,21 +23,6 @@ export default function Navbar() {
       <div className="flex gap-4 justify-end">
         {loading ? null : user ? (
           <>
-            <Select onValueChange={handleSelect}>
-              <SelectTrigger className="p-6" size="sm">
-                <SelectValue placeholder="My Organizations" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  {user?.orgs?.map((org) => (
-                    <SelectItem value={org.id.toString()}>
-                      {org.name}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-
             <DropdownMenu>
               <DropdownMenuTrigger>
                 <img
@@ -60,7 +31,11 @@ export default function Navbar() {
                   className="w-12 h-12"
                 />
               </DropdownMenuTrigger>
-              <DropdownMenuContent>
+              <DropdownMenuContent className="w-64">
+                <DropdownMenuLabel>
+                  <p>{user.name}</p>
+                  <p className="opacity-50">{user.email}</p>
+                </DropdownMenuLabel>
                 <DropdownMenuItem>My Organizations</DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <form action="/api/logout" method="POST">
