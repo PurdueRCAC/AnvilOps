6109fb1e037b88ffd87bb8d32b7737552ace46af
Emma Zheng <73250770+skkra0@users.noreply.github.com>
2025-06-20 11:47:50 -0400

    Add Organizations to nav

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -33,6 +33,9 @@ export default function Navbar() {
             <Link to="/dashboard" className="sm:text-white">
               Dashboard
             </Link>
+            <Link to="/organizations" className="sm:text-white">
+              Organizations
+            </Link>
           </>
         )}
         {loading ? null : user ? (
