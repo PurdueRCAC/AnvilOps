be38337f58d2f9e825620a8f7a407a66f1ab259f
swans150 <swans150@purdue.edu>
2025-09-16 22:38:53 -0400

    Fix BuildKit daemon address

--- builders/railpack/docker-entrypoint.sh
+++ builders/railpack/docker-entrypoint.sh
@@ -37,7 +37,6 @@ run_job() {
       --export-cache type=registry,ref="$CACHE_TAG" \
       --import-cache type=registry,ref="$CACHE_TAG" \
       --output type=image,name="$IMAGE_TAG",push=true
-    # TODO: when adding support for secrets, remember to invalidate the cache when their values change: https://railpack.com/guides/running-railpack-in-production/#layer-invalidation
   }
 
   if build ; then
