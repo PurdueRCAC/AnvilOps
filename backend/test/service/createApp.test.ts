1ed280794005aac31595d77e80a3b62b88eacc1e
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-12-20 18:02:33 -0500

    Add more asserts to createApp test

--- backend/test/service/createApp.test.ts
+++ backend/test/service/createApp.test.ts
@@ -1,13 +1,65 @@
-import { assert, describe, test } from "vitest";
+import {
+  ApiException,
+  AppsV1Api,
+  KubeConfig,
+  type ApiConstructor,
+} from "@kubernetes/client-node";
+import { setTimeout } from "node:timers/promises";
+import { describe, expect, test } from "vitest";
 import { db } from "../../src/db/index.ts";
 import type { User } from "../../src/db/models.ts";
+import { DeploymentStatus } from "../../src/generated/prisma/enums.ts";
+import { getNamespace } from "../../src/lib/cluster/resources.ts";
 import {
   createApp,
   validateAppConfig,
   type NewApp,
 } from "../../src/service/createApp.ts";
 import { deleteOrgByID } from "../../src/service/deleteOrgByID.ts";
+import { getAppByID } from "../../src/service/getAppByID.ts";
 import { getTestNamespace, getTestUser } from "../fixtures/user.ts";
+
+const kc = new KubeConfig();
+kc.loadFromDefault();
+
+async function waitForCreate<T, C>(
+  clientType: ApiConstructor<C>,
+  check: (client: C) => Promise<T>,
+): Promise<T> {
+  let last404Error: ApiException<unknown>;
+  const client = kc.makeApiClient(clientType);
+  for (let i = 0; i < 20; i++) {
+    try {
+      return await check(client);
+    } catch (e) {
+      if (e instanceof ApiException && e.code === 404) {
+        // Not found; continue waiting
+        last404Error = e;
+        await setTimeout(500);
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  throw new Error("Timed out waiting for resource", { cause: last404Error });
+}
+
+async function waitForStatusCode(url: string, code: number) {
+  let lastError: Error;
+  for (let i = 0; i < 20; i++) {
+    try {
+      const response = await fetch(url);
+      if (response.status === code) {
+        return response;
+      }
+    } catch (e) {
+      lastError = e;
+    }
+    await setTimeout(500);
+  }
+  throw new Error("Timed out waiting for HTTP status", { cause: lastError });
+}
 
 describe("createApp", async (c) => {
   let user: User, orgId: number;
@@ -29,29 +81,59 @@ describe("createApp", async (c) => {
     createApp(config, await validateAppConfig(user.id, config));
 
   test("from Docker image", async () => {
-    const appId = await create({
+    const config = {
       appGroup: { type: "standalone" },
       source: "image",
       imageTag: process.env.TEST_ANVILOPS_SAMPLE_IMAGE,
       cpuCores: 1,
       memoryInMiB: 512,
-      createIngress: false,
+      createIngress: true,
       env: [],
       mounts: [],
       name: "test-app",
       orgId,
-      port: 80,
+      port: 8080,
       subdomain: getTestNamespace(),
+    } satisfies NewApp;
+
+    const appId = await create(config);
+    const app = await getAppByID(appId, user.id);
+    const ns = getNamespace(app.namespace);
+
+    const sts = await waitForCreate(AppsV1Api, (c) =>
+      c.readNamespacedStatefulSet({
+        namespace: ns,
+        name: app.name,
+      }),
+    );
+
+    const deployment = await db.app.getMostRecentDeployment(app.id);
+
+    expect(deployment.status).toEqual(DeploymentStatus.COMPLETE);
+
+    expect(sts.spec.template.metadata.labels).toEqual({
+      "anvilops.rcac.purdue.edu/app-group-id": app.appGroup.id.toString(),
+      "anvilops.rcac.purdue.edu/app-id": app.id.toString(),
+      "anvilops.rcac.purdue.edu/deployment-id": deployment.id.toString(),
+      "app.kubernetes.io/managed-by": "anvilops",
+      "app.kubernetes.io/name": app.name,
+      "app.kubernetes.io/part-of": `${app.appGroup.name}-${app.appGroup.id}-${app.orgId}`,
+      app: app.name,
     });
 
-    test("from Dockerfile", () => {
-      assert.fail("Not implemented yet");
-    });
+    expect(sts.spec.template.spec.containers[0].image).toEqual(config.imageTag);
 
-    test("from Railpack", () => {
-      assert.fail("Not implemented yet");
-    });
+    const clusterInternalURL = `http://${ns}.${ns}.svc.cluster.local`;
 
-    console.log(appId);
-  });
+    const response = await waitForStatusCode(clusterInternalURL, 200);
+    expect(await response.text()).toEqual("Hello, world!\n");
+  }, /* timeout = */ 60_000);
+
+  // test("from Dockerfile", () => {
+  //   assert.fail("Not implemented yet");
+  // });
+
+  // test("from Railpack", () => {
+  //   assert.fail("Not implemented yet");
+  // });
 });
