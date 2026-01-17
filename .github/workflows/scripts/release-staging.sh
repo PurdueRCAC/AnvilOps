c87d7765be0894fdc734d76de9e4353127f8b7b4
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2026-01-17 16:31:46 -0500

    Update staging URLs

--- .github/workflows/scripts/release-staging.sh
+++ .github/workflows/scripts/release-staging.sh
@@ -3,12 +3,11 @@
 # This script releases a new version to staging.
 # Usage: ./release-staging.sh
 
-# TODO update these to use proper staging repo
-export REGISTRY_BASE="registry.anvil.rcac.purdue.edu/anvilops-brendan-dev"
-export HELM_ARTIFACT_TAG="oci://registry.anvil.rcac.purdue.edu/anvilops-brendan-dev/chart"
+export REGISTRY_BASE="registry.anvil.rcac.purdue.edu/anvilops-staging"
+export HELM_ARTIFACT_TAG="oci://registry.anvil.rcac.purdue.edu/anvilops-staging/chart"
 # note that GENERATE_GITHUB_RELEASE is not exported in this script, but it is in release-production.sh
 
-VERSION="$(date +%s)-$(git rev-parse HEAD)"
+VERSION="0.0.0-staging.$(date +%s)-$(git rev-parse HEAD)"
 
 CURRENT_DIR=$(dirname "$0")
-$CURRENT_DIR/release.sh "0.0.0-staging.$VERSION"
+$CURRENT_DIR/release.sh "$VERSION"
